d1aac9f082daa17ed39945b8a2432d6892d9880e
Renzo Trabucco <renzotrabuccolissarrague@gmail.com>
2024-05-17 19:37:05 -0300

    metodo toggleTodo implementado

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Piedra del amla'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,
 }
@@ -50,7 +52,12 @@ const addTodo = ( description ) => {
 }
 
 const toggleTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 }
 
 const deleteTodo = ( todoId ) => {
@@ -79,11 +86,11 @@ export default {
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,addTodo,
     setFilter,
     toggleTodo,
-    getTodos,
 }
 
 
